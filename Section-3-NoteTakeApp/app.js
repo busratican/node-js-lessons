97a8d859327350848f68854b5eb573796b8ba161
busratican <gulbusra55@gmail.com>
2023-06-21 20:17:35 +0000

    Npm Package Manager Global packages works

--- Section-3-NoteTakeApp/app.js
+++ Section-3-NoteTakeApp/app.js
@@ -1,4 +1,7 @@
 //load fs module
+const validator = require('validator')
+var validate = require("validate-npm-package-name")
+
 const {add, getNotes} = require("./utils")
 const sum = add(2,3)
 console.log(sum)
@@ -6,4 +9,7 @@ const notes = getNotes("Busra")
 console.log(notes)
 
 
+console.log(validator.isEmail('andrewexample.com'))
+console.log(validator.isURL('andrewexample.m'))
+console.log(validate('excited!'))
 
